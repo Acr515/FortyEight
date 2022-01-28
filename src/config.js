8e0f93e63020d55d9280c83f9cf55814b368c3d1
Alex Rummel <rummelalexander@outlook.com>
2022-01-27 21:33:29 -0500

    Bump to 2022.1.0

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const VERSION_NAME = "2022.0.0";
+export const VERSION_NAME = "2022.1.0";
 
-export const VERSION_NUMBER = 2022.0;
+export const VERSION_NUMBER = 2022.1;
 
-export const PACKAGE_VERSION_NUMBER = "1.0.0";
+export const PACKAGE_VERSION_NUMBER = "1.1.0";
