5f6465b72fb7f393b378473ec0b063e10fa3a5d5
Acr515 <rummelalexander@outlook.com>
2026-03-12 22:00:21 -0400

    Added simulator insight row set

---
+++ src/components/game_specific/SimulatorInsightRowSet/2026.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import SimulatorInsightRow from "components/SimulatorInsightRow";
+import ScoreCalculator from "data/game_specific/ScoreCalculator/2026";
+
+export default function SimulatorInsightRowSet({sim, winner, loser}) {
+    
+    // Find best fuel scorers
+    let winnerBestScorer = -1, loserBestScorer = -1;
+    const getBestPieceScorers = teamColor => {
+        let bestTeleopScore = -100;
+        let bestScorer = -1;
+        sim.averageMatch[teamColor].teamPerformances.forEach(team => {
+            let score = ScoreCalculator.Teleop.getScore({ performance: team });
+
+            if (score > bestTeleopScore) {
+                bestTeleopScore = score;
+                bestScorer = team.teamNumber;
+            }
+        });
+        return bestScorer;
+    };
+    winnerBestScorer = getBestPieceScorers(winner.colorName);
+    loserBestScorer = getBestPieceScorers(loser.colorName);
+
+    return <>
+        <SimulatorInsightRow
+            label="Strongest Piece Scorer"
+            winnerValue={winnerBestScorer}
+            winnerColor={winner.color}
+            loserValue={loserBestScorer}
+            loserColor={loser.color}
+            hyperlinkTeams
+        />
+    </>
+}
